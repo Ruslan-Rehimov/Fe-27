8a1ebfd085b3eb83da633736f30dafe1d43ec176
TojiZenin041 <145575696+TojiZenin041@users.noreply.github.com>
2024-04-25 19:22:57 +0400

    Add files via upload

---
+++ FE27/js fe 27/evin kvadrati/app.js
@@ -0,0 +1,14 @@
+let qiymet = 99999
+let valyuta = "azn"
+let kvadrat = 2000
+
+qiymet = +prompt("evin qiymetini daxil edin");
+valyuta = prompt("valyutani daxil edin");
+kvadrat = +prompt("evin kvadratini daxil edin")
+
+
+if(qiymet === 99999 && valyuta === "azn" && kvadrat === 2000 ) {
+    document.write("Evi aliram")
+} else{
+    document.write("Evi almiram")
+};
